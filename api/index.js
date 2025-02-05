ec6f8409614acad74bcebf127fff6119ff73ce45
Lyndsey Herbert <lyndsherb@gmail.com>
2025-02-05 18:19:32 +0000

    Remove workspaces

--- api/index.js
+++ api/index.js
@@ -4,12 +4,14 @@ import routes from './routes.js';
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 app.use(routes);
 
 app.get('/', (_, res) => {
-  res.send('yippie');
+  res.send('woohoo');
 });
 
 app.listen(port, () => {
+  console.log('hi!');
   console.log(`Example app listening on port ${port}`);
 });
