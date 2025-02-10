1307daf2f352eaade5d9f7c50c9365487674c1a9
Lyndsey Herbert <lyndsherb@gmail.com>
2025-02-10 04:33:04 +0000

    Update base sample data, ensure Next can build

--- events/src/app/events/[slug]/page.tsx
+++ events/src/app/events/[slug]/page.tsx
@@ -2,6 +2,18 @@ import classNames from 'classnames';
 import { formatDate } from '@/utils/formatDate';
 import { getSingleEvent } from '@/utils/getData';
 
+// if this was to go into production we would NOT do this - otherwise the page cannot be indexed
+// and that means the site would go right down in Google results. Bad SEO is a bad time!
+export const dynamic = 'force-dynamic';
+
+export function generateStaticParams() {
+  return [
+    {
+      slug: '04de7ba9-7d86-46e6-a93a-0b977dd7f585',
+    },
+  ];
+}
+
 export default async function Event({
   params,
 }: {
--- events/src/app/events/page.tsx
+++ events/src/app/events/page.tsx
@@ -2,6 +2,10 @@ import Link from 'next/link';
 import { getEvents } from '@/utils/getData';
 import { formatDate } from '@/utils/formatDate';
 
+// if this was to go into production we would NOT do this - otherwise the page cannot be indexed
+// and that means the site would go right down in Google results. Bad SEO is a bad time!
+export const dynamic = 'force-dynamic';
+
 export default async function Events() {
   const events = await getEvents();
 
